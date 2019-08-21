fb9169cc850a892ad2acf8f713fd29e95c62eb92
Galaxy_G <269274122@qq.com>
2019-08-21 22:33:16 +0800

    初始化

---
+++ pages/tabBar/dashboard/contractDetail/contractDetail.vue
@@ -0,0 +1,137 @@
+<template>
+<view class="example listBg">
+    <view style="padding-top:20px;padding-bottom:40px;"">
+        <media-list :options=" newsitem" @close="close(index1,index2)">
+        </media-list>
+
+        <view class="list-cell " hover-class="uni-list-cell-hover" @click="bindClick" style="margin: 0 25px  0 25px;background:#fff">
+            <view class="media-list" style="margin: 0px 25px 5px 25px;border-top: 1px solid#ccc;">
+                <view class="uni-triplex-left" style="width:100%;padding:20px 0 ">
+                    <text class="uni-text">销售联系人:张三 <text class="uni-h5" style="padding-left:15px;"> 联系电话:12381233322</text></text>
+                </view>
+            </view>
+
+            <view style="padding:10px 0" @click="goDetail(newsitem)">
+                <view class="media-list" style="display: flex;width: 100%;">
+                    <view class="uni-list" style="flex: 6;">
+                        <view class="uni-list-cell" hover-class="uni-list-cell-hover">
+                            <view class="uni-media-list">
+                                <view class="uni-media-list-logo">
+                                    <image :src="newsitem.image_url"></image>
+                                </view>
+                                <view class="uni-media-list-body">
+                                    <view class="uni-media-list-text-top">{{newsitem.title}}</view>
+                                    <view class="uni-media-list-text-bottom uni-ellipsis">{{newsitem.title}}</view>
+                                </view>
+                            </view>
+                        </view>
+                    </view>
+                    <view style="flex: 2;">
+                        <view v-show="newsitem.comment_count == 0" class="rcircle_blue">完</view>
+                        <view v-show="newsitem.comment_count == 1" class="rcircle_red">未</view>
+                    </view>
+                </view>
+                <view class="progress-box" style="padding:0px 65px 0px 25px;color: #57bde5;">
+                    <progress percent="80" activeColor="#57bce8" backgroundColor="#e5e6ea" active stroke-width="10" style='border-radius:5px;overflow:hidden;'></progress>
+                    <div class="" style="float:right;position: relative;top: -18px;left: 50px;">80%</div>
+                </view>
+            </view>
+            <view style="padding:10px 0" @click="goDetail(newsitem)">
+                <view class="media-list" style="display: flex;width: 100%;">
+                    <view class="uni-list" style="flex: 6;">
+                        <view class="uni-list-cell" hover-class="uni-list-cell-hover">
+                            <view class="uni-media-list">
+                                <view class="uni-media-list-logo">
+                                    <image :src="newsitem.image_url"></image>
+                                </view>
+                                <view class="uni-media-list-body">
+                                    <view class="uni-media-list-text-top">{{newsitem.title}}</view>
+                                    <view class="uni-media-list-text-bottom uni-ellipsis">{{newsitem.title}}</view>
+                                </view>
+                            </view>
+                        </view>
+                    </view>
+                    <view style="flex: 2;">
+                        <view v-show="newsitem.comment_count == 0" class="rcircle_blue">完</view>
+                        <view v-show="newsitem.comment_count == 1" class="rcircle_red">未</view>
+                    </view>
+                </view>
+                <view class="progress-box" style="padding:0px 65px 0px 25px;color: #57bde5;">
+                    <progress percent="80" activeColor="#57bce8" backgroundColor="#e5e6ea" active stroke-width="10" style='border-radius:5px;overflow:hidden;'></progress>
+                    <div class="" style="float:right;position: relative;top: -18px;left: 50px;">80%</div>
+                </view>
+            </view>
+            <view style="padding:10px 0" @click="goDetail(newsitem)">
+                <view class="media-list" style="display: flex;width: 100%;">
+                    <view class="uni-list" style="flex: 6;">
+                        <view class="uni-list-cell" hover-class="uni-list-cell-hover">
+                            <view class="uni-media-list">
+                                <view class="uni-media-list-logo">
+                                    <image :src="newsitem.image_url"></image>
+                                </view>
+                                <view class="uni-media-list-body">
+                                    <view class="uni-media-list-text-top">{{newsitem.title}}</view>
+                                    <view class="uni-media-list-text-bottom uni-ellipsis">{{newsitem.title}}</view>
+                                </view>
+                            </view>
+                        </view>
+                    </view>
+                    <view style="flex: 2;">
+                        <view v-show="newsitem.comment_count == 0" class="rcircle_blue">完</view>
+                        <view v-show="newsitem.comment_count == 1" class="rcircle_red">未</view>
+                    </view>
+                </view>
+                <view class="progress-box" style="padding:0px 65px 0px 25px;color: #57bde5;">
+                    <progress percent="80" activeColor="#57bce8" backgroundColor="#e5e6ea" active stroke-width="10" style='border-radius:5px;overflow:hidden;'></progress>
+                    <div class="" style="float:right;position: relative;top: -18px;left: 50px;">80%</div>
+                </view>
+            </view>
+
+
+        </view>
+    </view>
+</view>
+</template>
+<script>
+import mediaList from '@/components/tab-nvue/mediaList.vue';
+export default {
+    components: {
+        mediaList
+    },
+    data() {
+        return {
+            newsitem: {
+                "datetime": "40分钟前",
+                "article_type": 0,
+                "title": "不锈钢管订购合同1",
+                "source": "订单号：000002352356",
+                "image_url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+                "comment_count": 0
+            },
+        }
+    },
+    onLoad(e) {
+        this.title = e.title || '';
+    },
+    methods: {
+        goDetail(e) {
+            uni.navigateTo({
+                url: '/pages/tabBar/dashboard/orderDetail/orderDetail?title=' + e.title
+            });
+        },
+
+    }
+}
+</script>
+<style>
+/* page {
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    background:linear-gradient(top,rgb(49, 147, 186,1) 1%,rgb(117, 216, 250) 99%);
+    background-size:100%
+} */
+.listBg {
+    background: linear-gradient(top, rgb(49, 147, 186, 1) 1%, rgb(117, 216, 250) 99%);
+}
+</style>
---
+++ pages/tabBar/dashboard/orderDetail/orderDetail.vue
@@ -0,0 +1,120 @@
+<template>
+<view class="example listBg">
+
+    <view style="padding-top:20px;padding-bottom:40px;"">
+    <view class="" style=" display:flex;width: 100%;">
+        <view class="" style="flex:1;display: flex;align-items: center; justify-content: center; " @click="swiper_index = 0">
+            <text class="iconfont">&#xe606;{{swiper_index}}</text>
+        </view>
+
+        <view class="uni-margin-wrap" style="flex:10">
+            <swiper class="swiper" :current="swiper_index" v-model="swiper_index" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration" @change="asd">
+                <swiper-item>
+                    <media-list :options="newsitem" @close="close(index1,index2)"></media-list>
+                </swiper-item>
+                <swiper-item>
+                    <media-list :options="newsitem" @close="close(index1,index2)"></media-list>
+                </swiper-item>
+                <swiper-item>
+                    <media-list :options="newsitem" @close="close(index1,index2)"></media-list>
+                </swiper-item>
+            </swiper>
+        </view>
+        <view class="" style="flex:1;display: flex;align-items: center; justify-content: center; " @click="swiper_index = 1">
+            <text class="iconfont">&#xe64b;{{swiper_index}}</text>
+        </view>
+    </view>
+
+
+    <uni-steps :options="list2" :active="active" direction="column" />
+</view>
+</view>
+</template>
+<script>
+import mediaList from '@/components/tab-nvue/mediaList.vue';
+import uniSteps from '@/components/uni-steps/uni-steps.vue'
+export default {
+    components: {
+        mediaList,
+        uniSteps
+    },
+    data() {
+        return {
+            title: '',
+            active: 1,
+            list2: [{
+                title: '买家下单',
+                desc: '2018-11-11'
+            }, {
+                title: '卖家发货',
+                desc: '2018-11-12'
+            }, {
+                title: '买家签收',
+                desc: '2018-11-13'
+            }, {
+                title: '交易完成',
+                desc: '2018-11-14'
+            }],
+            newsitem: {
+                "datetime": "40分钟前",
+                "article_type": 0,
+                "title": "不锈钢管订购合同1",
+                "source": "订单号：000002352356",
+                "image_url": "https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg?imageView2/3/w/200/h/100/q/90",
+                "comment_count": 0
+            },
+            background: ['color1', 'color2', 'color3'],
+            indicatorDots: false,
+            autoplay: false,
+            interval: 2000,
+            duration: 500,
+            swiper_index: 0
+        }
+    },
+    onLoad(e) {
+        this.title = e.title || '';
+    }
+}
+</script>
+<style>
+/* page {
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    background: linear-gradient(top, rgb(49, 147, 186, 1) 1%, rgb(117, 216, 250) 99%);
+} */
+.listBg {
+    background: linear-gradient(top, rgb(49, 147, 186, 1) 1%, rgb(117, 216, 250) 99%);
+}
+
+.uni-margin-wrap {
+    width: 490upx;
+    margin: 0 auto;
+}
+
+.swiper {
+    height: 700upx;
+}
+
+.swiper-item {
+    display: block;
+    height: 700upx;
+    line-height: 700upx;
+    text-align: center;
+}
+
+.swiper-list {
+    margin-top: 40upx;
+    margin-bottom: 0;
+}
+
+.uni-common-mt {
+    margin-top: 60upx;
+    position: relative;
+}
+
+.info {
+    position: absolute;
+    right: 20upx;
+}
+</style>
